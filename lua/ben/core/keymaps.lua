40a08f2517206246eef59c0d5e4bbcd002bc509a
Ben Fields <techpotato22@gmail.com>
2023-10-01 16:45:25 -0700

    Updated for mac setup.

---
+++ lua/ben/core/keymaps.lua
@@ -0,0 +1,25 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+keymap.set("n", "<C-s>", ":w<CR>")
+-- use jk to exit insert mode
+keymap.set("i", "jj", "<ESC>l")
+keymap.set("i", "jk", "<ESC>la")
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "x", '"_x')
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+keymap.set("n", "<tab>", ":tabn<CR>") --  go to previous tab
+keymap.set("n", "<S-tab>", ":tabp<CR>") --  go to previous tab
+
+keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>") -- toggle file explorer
---
+++ lua/ben/core/options.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt
+local vg = vim.g
+local env = vim.env
+opt.relativenumber = true
+opt.number = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+opt.termguicolors = true
+
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
+
+opt.splitright = true
+opt.splitbelow = true
+
